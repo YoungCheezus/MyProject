5678e2694fcd57f8abbc1a6bc7ecfe1f14f58eba
Cheezus <maksimgrigoriev98.02@gmail.com>
2020-08-13 00:45:42 +0700

    First Files Added 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-alert('Hellooo');
+﻿function f1() {
+    let div = document.querySelector('#one');
+    div.innerHTML = div.innerHTML + ' Hello';
+}
+f1();
+
+let c = document.querySelector('#two');
+function f2() {
+    c.innerHTML += ' Hello';
+
+}
+f2();
+c = document.querySelector('#three');
+f2();
+
+function f4(a) {
+    let d = document.querySelector('#' + a);
+    d.innerHTML += ' Eee';
+}
+f4('one');
+f4('two');
+function summ(a,b=3){
+    return a*b;
+}
+console.log( summ(3));
--- js/script2.js
+++ js/script2.js
@@ -1,1 +1,16 @@
-alert('54312');
+﻿function MyClick() {
+    console.log('click');
+  //  console.log(event.target);
+    console.log(this);
+}
+// document.querySelector('#one').onclick = MyClick;
+
+document.onclick = function (event) {
+    if (event.target.id == 'one') {
+        console.log ('нажали на one');
+    } else if (event.target.id == 'two'){
+        console.log('нажили на two');
+        } else { 
+        console.log('Нажили на three');
+    }
+}
